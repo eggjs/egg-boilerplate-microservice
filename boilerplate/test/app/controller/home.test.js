e3c6f6aca007b6cceb91e6a77d1e12bf0476766c
zōng yǔ <gxcsoccer@users.noreply.github.com>
2018-11-03 04:52:28 +0800

    feat: use egg-cloud instead of sofa-node (#4)

--- boilerplate/test/app/controller/home.test.js
+++ boilerplate/test/app/controller/home.test.js
@@ -7,7 +7,12 @@ describe('test/app/controller/home.test.js', () => {
   it('should GET /', () => {
     return app.httpRequest()
       .get('/')
-      .expect('hi, sofa-node')
+      .expect({
+        id: 123456
+        name: '宗羽',
+        address: '蚂蚁 C 空间',
+        salary: 100000000,
+      })
       .expect(200);
   });
 });
